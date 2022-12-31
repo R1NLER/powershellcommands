f493f32f3f8d81abc6e18bd4d03e28e23092a6cb
RINLER <90422888+R1NLER@users.noreply.github.com>
2022-12-31 17:50:22 +0100

    Add files via upload

---
+++ server2019_standard_activate.bat
@@ -0,0 +1,4 @@
+DISM /online /Set-Edition:ServerStandard /ProductKey:N69G4-B89J2-4G8F4-WWYCC-J464C /AcceptEula
+slmgr.vbs /ipk N69G4-B89J2-4G8F4-WWYCC-J464C
+slmgr.vbs /skms kms8.msguides.com
+slmgr.vbs /ato
---
+++ server2022_standard_activate.bat
@@ -0,0 +1,4 @@
+DISM /online /Set-Edition:ServerStandard /ProductKey:VDYBN-27WPP-V4HQT-9VMD4-VMK7H /AcceptEula
+slmgr.vbs /ipk VDYBN-27WPP-V4HQT-9VMD4-VMK7H
+slmgr.vbs /skms kms8.msguides.com
+slmgr.vbs /ato
